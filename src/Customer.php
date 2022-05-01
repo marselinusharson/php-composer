9a42f4b4798317c6872961fd0ff20ba7ca91cda0
marselinusharson <marselinus@undiksha.ac.id>
2022-05-01 16:05:22 +0800

    initial commit contoh library composer

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MarselinusHarsonRewo\Belajar{
+
+    class Customer{
+
+        public function __construct(private string $name){
+
+        }
+
+        public function sayHello(string $name):string{
+            return "Hello $name, My name is $this->name".PHP_EOL;
+        }
+    }
+}
